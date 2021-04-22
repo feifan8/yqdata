28dd4b3b11c182a41e6cc276db8784b9f46b9393
feifan8 <65496276+feifan8@users.noreply.github.com>
2021-04-22 16:44:41 +0800

    Add files via upload

---
+++ datasourse/DatasourseApplication.java
@@ -0,0 +1,15 @@
+package getData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling //开启定时任务
+public class DatasourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourseApplication.class, args);
+    }
+
+}
